0ace26badd5032b357484ff8ab1f9797cbe9aab5
Dominique <Sevendayes@gmail.com>
2020-09-23 16:24:31 +0200

    Mise en place de la structure

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,14 +2,15 @@
 import React from 'react';
 
 // == Import
-import reactLogo from './react-logo.svg';
 import './styles.css';
+import Form from 'src/components/Form';
+import Messages from 'src/components/Messages';
 
 // == Composant
 const App = () => (
   <div className="app">
-    <img src={reactLogo} alt="react logo" />
-    <h1>Composant : App</h1>
+    <Form />
+    <Messages />
   </div>
 );
 
---
+++ src/components/Messages/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import Message from 'src/components/Message';
+
+const Messages = () => (
+  <div>
+    <Message />
+  </div>
+);
+
+export default Messages;
